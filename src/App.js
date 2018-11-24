3e4495c7ca1120d34a62e0848b7217cc31d05e84
Thomas Lui <tlfit28@gmail.com>
2018-11-24 12:26:09 -0500

    Ether integration

--- src/App.js
+++ src/App.js
@@ -1,24 +1,129 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from 'ethers';
 
 class App extends Component {
+
+  constructor (props) {
+    super(props)
+
+    let url = "http://localhost:8545";
+    let provider = new ethers.providers.JsonRpcProvider(url);
+
+    let privateKey = "";
+    let walletWithProvider = new ethers.Wallet(privateKey, provider);
+
+    let abi =
+    [
+                	{
+                		"constant": false,
+                		"inputs": [],
+                		"name": "getMessage",
+                		"outputs": [
+                			{
+                				"name": "",
+                				"type": "string"
+                			}
+                		],
+                		"payable": false,
+                		"stateMutability": "nonpayable",
+                		"type": "function"
+                	},
+                	{
+                		"anonymous": false,
+                		"inputs": [
+                			{
+                				"indexed": false,
+                				"name": "newMessage",
+                				"type": "string"
+                			}
+                		],
+                		"name": "MessageChanged",
+                		"type": "event"
+                	},
+                	{
+                		"constant": false,
+                		"inputs": [
+                			{
+                				"name": "newMsg",
+                				"type": "string"
+                			}
+                		],
+                		"name": "setMessage",
+                		"outputs": [],
+                		"payable": false,
+                		"stateMutability": "nonpayable",
+                		"type": "function"
+                	},
+                	{
+                		"constant": true,
+                		"inputs": [],
+                		"name": "message",
+                		"outputs": [
+                			{
+                				"name": "",
+                				"type": "string"
+                			}
+                		],
+                		"payable": false,
+                		"stateMutability": "view",
+                		"type": "function"
+                	}
+    ]
+
+    let contractAddress = "";
+    let contract = new ethers.Contract(contractAddress, abi, walletWithProvider);
+    this.contract = contract;
+
+    this.state = {
+      message: '',
+      input: ''
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  async componentDidMount() {
+    const message = await this.contract.message();
+    this.setState((state) => {
+      return {message: message};
+    });
+  }
+
+  handleInputChange = (e) => {
+    this.setState({input: e.target.value});
+  }
+
+
+  async handleClick() {
+    await this.contract.setMessage(this.state.input);
+    let message = await this.contract.message();
+
+    this.setState((state) => {
+      return {message: message};
+    });
+
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Edit <code>src/App.js</code> and save to reload.
+            {this.state.message}
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
+          <input
+          onChange={this.handleInputChange}
+          type="text"
+          name="name"
+          value={this.state.input}/>
+          <button onClick={this.handleClick}>
+            Update Message
+          </button>
+
         </header>
       </div>
     );
